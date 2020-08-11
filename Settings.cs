64f18f1f7d7940e7e565d19a2b28d7f3299b759a
Alex Lopes <alexlr10@hotmail.com>
2020-08-11 16:06:52 -0300

    Adicionar arquivos de projeto.

---
+++ Settings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop {
+    public static class Settings {
+        public static string Secret = "f6asd54f6asd4f6asd4f";
+    }
+}
